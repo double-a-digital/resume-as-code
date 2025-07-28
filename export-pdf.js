f964463f7a2ae4be5127056919d0cee9fdb76801
opariffazman <ariff.azman@proton.me>
2025-07-28 22:46:20 +0800

    feat: added support to accept filePath as arguments when exporting

--- export-pdf.js
+++ export-pdf.js
@@ -1,26 +1,27 @@
 const { chromium } = require("playwright")
 const path = require("path")
+const filePath = process.env.filePath || "."
 
-async function generatePdfFromFile() {
+async function generatePdfFromFile(filePath) {
   const browser = await chromium.launch()
   const page = await browser.newPage()
 
-  const htmlFilePath = path.join(__dirname, "index.html")
+  const htmlFilePath = path.join(__dirname, `${filePath}/index.html`)
   const fileUrl = `file://${htmlFilePath}`
 
   console.log(`Loading HTML from: ${fileUrl}`)
   await page.goto(fileUrl)
 
   await page.pdf({
-    path: "resume.pdf",
+    path: `${filePath}/resume.pdf`,
     preferCSSPageSize: true,
   })
 
   console.log(
-    "PDF generated successfully from local file: resume.pdf"
+    `PDF generated successfully from ${filePath}/index.html as ${filePath}/resume.pdf 🚀`
   )
 
   await browser.close()
 }
 
-generatePdfFromFile()
+generatePdfFromFile(filePath)
